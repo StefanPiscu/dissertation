91bffe47a7e4d0ab69db640e56863d9e1eee5e6d
Mathieu Fehr <mathieu.fehr@gmail.com>
2026-05-11 21:14:48 -0700

    Mathieu's comments on preparation

--- compile.sh
+++ compile.sh
@@ -1,2 +1,13 @@
-latexmk -pdf -shell-escape -synctex=1 -interaction=nonstopmode -file-line-error -outdir=build main.tex > /dev/null
+#!/bin/bash
+
+# minted v3 removed the outputdir package option. Create a temporary shim
+# that downgrades the fatal error to a warning so the option is silently ignored.
+SYSTEM_MINTED=$(kpsewhich minted.sty)
+MINTED_COMPAT_DIR=$(mktemp -d)
+sed 's/outputdir\/.code=\\minted@error{/outputdir\/.code=\\minted@warning{/' "$SYSTEM_MINTED" > "$MINTED_COMPAT_DIR/minted.sty"
+
+TEXINPUTS="$MINTED_COMPAT_DIR:" latexmk -pdf -shell-escape -synctex=1 -interaction=nonstopmode -file-line-error -outdir=build main.tex > /dev/null
+
+rm -rf "$MINTED_COMPAT_DIR"
+
 mv build/main.pdf main.pdf
